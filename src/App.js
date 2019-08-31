e46d382f2c0174b2b7c8ecfe9273b0eb31538408
RAHUL AGARWAL <rahulchamp2000@gmail.com>
2019-08-31 13:17:19 +0530

    chnged the modal style and closing the modal

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import {Link,Switch} from "react-router-dom"
 import Invalid from "./components/invalid"
 import logo from "./logo.svg"
 import Details from "./components/details"
+import {ProductConsumer} from "./components/context"
 
 /*function App() {
   return (
@@ -35,10 +36,18 @@ function App()
   return(
     <React.Fragment>{/*The enclosing element and thus we don't use any extra element*/}
       <div className="row header" style={{backgroundColor:"#2a2a72",height:"8%",marginBottom:"10%"}}>
-        <div className="col-lg-2 col-md-2 col-sm-2 col-2 text-center" style={{marginTop:"0.4%",height:"8%"}}><Link to="/"><img src={logo}/></Link></div>
-        <div className="col-1" style={{marginTop:"0.8%",height:"8%"}}><Link to="/" exact strict><span style={{color:"#f3f3f3",fontSize:"15pt"}}>PRODUCTS</span></Link></div>
-        <div className="col-lg-6 col-md-6 col-sm-5 col-4" style={{height:"5%"}}></div>
-        <div className="col-lg-2 col-md-3 col-sm-3 col-4" style={{marginTop:"0.8%",marginBottom:"9%"}}><Link to="/cart" exact strict><button className="btn btn-outline-info"><span className="fas fa-shopping-cart"></span> MY CART</button></Link></div>
+        <ProductConsumer>
+          {(object)=>{
+            return(
+              <React.Fragment>
+                <div className="col-lg-2 col-md-2 col-sm-2 col-2 text-center" style={{marginTop:"0.4%",height:"8%"}}><Link to="/"><img src={logo} onClick={()=>{object.modalclose();}}/></Link></div>
+                <div className="col-1" style={{marginTop:"0.8%",height:"8%"}}><Link to="/" exact strict><span style={{color:"#f3f3f3",fontSize:"15pt"}} onClick={()=>{object.modalclose();}}>PRODUCTS</span></Link></div>
+                <div className="col-lg-6 col-md-6 col-sm-5 col-4" style={{height:"5%"}}></div>
+                <div className="col-lg-2 col-md-3 col-sm-3 col-4" style={{marginTop:"0.8%",marginBottom:"9%"}}><Link to="/cart" exact strict><button className="btn btn-outline-info" onClick={()=>{object.modalclose();}}><span className="fas fa-shopping-cart"></span> MY CART</button></Link></div>
+              </React.Fragment>
+            )
+          }}
+        </ProductConsumer>    
       </div>  {/*This part will always be displayed in the page and when we click the products or my cart option then only the rest of the page chnages and the header remains intact coz only that route is rendered and not whole page is rendered again*/}
       <Switch>
         <Route path="/" exact strict render={()=>{
--- src/components/modal.js
+++ src/components/modal.js
@@ -14,17 +14,19 @@ function Modal()
                 {
                     return(
                         <React.Fragment>
-                            <div style={{boxShadow:"0.08rem 0.08rem 0.08rem 0.08rem silver",backgroundColor:"white",position:"fixed",bottom:"5%",left:"35%"}}>
-                                <br/>
-                                <div className="row"><div className="col-12 text-center"><b>ITEM ADDED TO CART</b></div></div>
-                                <div className="row"><div className="col-12 text-center"><img src={require(__dirname+`\/`+object.products[index].img)}/></div></div>
-                                <div className="row"><div className="col-12 text-center"></div></div>
-                                <div className="row"><div className="col-12 text-center" style={{marginTop:"0.6%"}}><b>{object.products[index].title}</b></div></div>
-                                <div className="row"><div className="col-12 text-center" style={{marginTop:"0.6%"}}><b>PRICE:{object.products[index].price}$</b></div></div>
-                                <div className="row"><div className="col-12 text-center" style={{marginTop:"0.6%"}}><Link to="/" exact><button className="btn btn-outline-info" onClick={()=>{object.modalclose()/*to close the modal once the link from modal is slected*/;}}>CONTINUE SHOPPING</button></Link></div></div>
-                                <div className="row"><div className="col-12 text-center" style={{marginTop:"0.6%"}}><Link to="/cart" exact><button className="btn btn-outline-warning" onClick={()=>{object.modalclose()/*to close the modal once the link from modal is slected*/;}}>GO TO CART</button></Link></div></div>
-                                <br/>
-                            </div>
+                            <div style={{backgroundColor:"lightgray",width:"100%",height:"100%",position:"fixed",bottom:"0.01%",right:"1%",opacity:"0.95"}}>
+                                <div style={{boxShadow:"0.08rem 0.08rem 0.08rem 0.08rem darkgray",backgroundColor:"#f3f3f3",position:"fixed",bottom:"5%",left:"35%"}}>
+                                    <br/>
+                                    <div className="row"><div className="col-12 text-center"><b>ITEM ADDED TO CART</b></div></div>
+                                    <div className="row"><div className="col-12 text-center"><img src={require(__dirname+`\/`+object.products[index].img)}/></div></div>
+                                    <div className="row"><div className="col-12 text-center"></div></div>
+                                    <div className="row"><div className="col-12 text-center" style={{marginTop:"0.6%"}}><b>{object.products[index].title}</b></div></div>
+                                    <div className="row"><div className="col-12 text-center" style={{marginTop:"0.6%"}}><b>PRICE:{object.products[index].price}$</b></div></div>
+                                    <div className="row"><div className="col-12 text-center" style={{marginTop:"0.6%"}}><Link to="/" exact><button className="btn btn-outline-info" onClick={()=>{object.modalclose()/*to close the modal once the link from modal is slected*/;}}>CONTINUE SHOPPING</button></Link></div></div>
+                                    <div className="row"><div className="col-12 text-center" style={{marginTop:"0.6%"}}><Link to="/cart" exact><button className="btn btn-outline-warning" onClick={()=>{object.modalclose()/*to close the modal once the link from modal is slected*/;}}>GO TO CART</button></Link></div></div>
+                                    <br/>
+                                </div>
+                            </div>    
                         </React.Fragment>
                     )
                 }    
